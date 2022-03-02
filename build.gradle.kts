e3276c6e316eea358b7462be745993fd31e352ef
Matheus Willock <matheuswillock@gmail.com>
2022-03-02 18:12:41 -0300

    Add: plugin flyway on the gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	id ("org.flywaydb.flyway") version "8.5.1"
 	id("org.springframework.boot") version "2.6.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
